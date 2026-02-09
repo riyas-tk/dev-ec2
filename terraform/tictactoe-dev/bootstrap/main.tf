2a6701f3d8f012731cbe7a514f1b24a02c30d183
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-02-09 08:13:14 +0530

    Modify for dev-ec2 repo

--- terraform/tictactoe-dev/bootstrap/main.tf
+++ terraform/tictactoe-dev/bootstrap/main.tf
@@ -1,6 +1,7 @@
 module "tictactoe_backend_setup" {
   source                 = "../../modules/bootstrap"
   region                 = "us-east-1"
+  github_repo            = "riyas-tk/dev-ec2"
   backend_s3_bucket      = "riyaz-tictactoe-backend-bucket-20260122"
   backend_dynamodb_table = "riyaz-backend-locker-dynamodb-table-20260122"
 }
